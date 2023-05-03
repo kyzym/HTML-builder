84bbf52377e45e2c267ef182dc74676cd314495b
kyzym <kyzym1@gmail.com>
2023-05-03 16:10:23 +0200

    add: solve 04-copy-directory 05/03/2023 16:10:23

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+const fromFolderPath = path.join(__dirname, 'files');
+const toFolderPath = path.join(__dirname, 'files-copy');
+
+const copyDir = async (src, dest) => {
+  try {
+    await fs.mkdir(dest, { recursive: true });
+
+    const files = await fs.readdir(src, { withFileTypes: true });
+
+    for (const file of files) {
+      const fromFilePath = path.join(src, file.name);
+      const toFilePath = path.join(dest, file.name);
+
+      if (file.isFile()) {
+        await fs.copyFile(fromFilePath, toFilePath);
+      }
+    }
+  } catch (err) {
+    console.error('Alarma,  Error:', err.message);
+  }
+};
+
+copyDir(fromFolderPath, toFolderPath);
